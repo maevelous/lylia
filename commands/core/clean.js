367d3c4632d88ce4b33f01151d64bb39b092dc7f
n3xistence <maxinies00@gmail.com>
2024-04-07 22:09:04 +0200

    fix(clean): restrict clean to admins only

--- commands/core/clean.js
+++ commands/core/clean.js
@@ -2,9 +2,13 @@ const { cleanChannel } = require("../../utils/misc")
 
 module.exports = {
   name: 'clean',
-  emphemeral: false,
+  emphemeral: true,
   description: "Clean the channel of all messages.",
   async execute({ client, inter }) {
+    if (!inter.member.permissions.has("ADMINISTRATOR"))
+      return inter.editReply({
+        content: "You do not have the required permissions to run this command.",
+      });
 
     cleanChannel(inter)
 
