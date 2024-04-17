86f6b8e0684b2fda6ce9c4c3f4432f96807ec23f
n3xistence <maxinies00@gmail.com>
2024-04-17 18:55:54 +0200

    fix(skip): fixed skip command response not being ephemeral

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,27 +1,33 @@
-const { EmbedBuilder } = require('discord.js');
-const { useMainPlayer, useQueue } = require('discord-player');
+const { EmbedBuilder } = require("discord.js");
+const { useMainPlayer, useQueue } = require("discord-player");
 
 module.exports = {
-  name: 'skip',
-  ephemeraa: true,
-  description: 'skip the track',
+  name: "skip",
+  ephemeral: true,
+  description: "skip the track",
   voiceChannel: true,
 
   execute({ inter }) {
-    const player = useMainPlayer()
+    const player = useMainPlayer();
 
     const queue = useQueue(inter.guild);
 
-    if (!queue || !queue.isPlaying()) return inter.editReply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+    if (!queue || !queue.isPlaying())
+      return inter.editReply({
+        content: `No music currently playing ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
     const success = queue.node.skip();
 
     const SkipEmbed = new EmbedBuilder()
-      .setColor('#2f3136')
-      .setAuthor({ name: success ? `Current music ${queue.currentTrack.title} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌` })
-
+      .setColor("#2f3136")
+      .setAuthor({
+        name: success
+          ? `Current music ${queue.currentTrack.title} skipped ✅`
+          : `Something went wrong ${inter.member}... try again ? ❌`,
+      });
 
     return inter.editReply({ embeds: [SkipEmbed] });
-
   },
 };
